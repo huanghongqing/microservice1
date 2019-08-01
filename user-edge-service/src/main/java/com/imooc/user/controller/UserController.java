a8e3e6a457c25b009731b28ae8a7a2fcacec51e2
huanghongqing <eclipseiscool@gmail.com>
2019-08-02 01:54:38 +0800

    edge service and data connection.

--- user-edge-service/src/main/java/com/imooc/user/controller/UserController.java
+++ user-edge-service/src/main/java/com/imooc/user/controller/UserController.java
@@ -1,15 +1,21 @@
 package com.imooc.user.controller;
 
 import com.imooc.thrift.user.UserInfo;
+import com.imooc.user.DTO.UserDTO;
+import com.imooc.user.redis.RedisClient;
+import com.imooc.user.response.LoginResponse;
 import com.imooc.user.response.Response;
 import com.imooc.user.thrift.ServiceProvider;
 import org.apache.thrift.TException;
 import org.apache.tomcat.util.buf.HexUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Random;
@@ -19,7 +25,11 @@ public class UserController {
 
     @Autowired
     private ServiceProvider serviceProvider;
+    @Autowired
+    private RedisClient redisClient;
+
     @RequestMapping(value="/login",method = RequestMethod.POST)
+    @ResponseBody
     public Response  login(@RequestParam("username") String username,
                       @RequestParam("password") String password){
         //1 验证用户名密码
@@ -34,14 +44,24 @@ public class UserController {
             return Response.USERNAME_PASSWORD_INVALID;
         }
         if(!userInfo.getPassword().equalsIgnoreCase(md5(password))){
+            System.out.println("Md5");
             return Response.USERNAME_PASSWORD_INVALID;
         }
         //2 生成token
         String token = genToken();
+
         //3 缓存用户和token
-        return null;
+        redisClient.set(token,toDTO(userInfo),3600);
+        return new LoginResponse(token);
 
     }
+
+    private UserDTO toDTO(UserInfo userInfo){
+        UserDTO  userDTO=new UserDTO();
+        BeanUtils.copyProperties(userInfo,userDTO);
+        return userDTO;
+    }
+
     private String genToken(){
 
         return randomCode("0123456789abcdefghijklmnopqrstuvwxyz",32);
@@ -51,7 +71,7 @@ public class UserController {
         StringBuffer result=new StringBuffer(size);
         Random random=new Random(s.length());
         for(int i=0;i<size;i++){
-            int loc=random.nextInt();
+            int loc=random.nextInt(s.length());
             result.append(s.charAt(loc));
         }
         return result.toString();
